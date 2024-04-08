399c35c5c07c6e798224b5404a1b4a45b044caa9
Tamara García <tamaragarciamarcos@gmail.com>
2024-04-08 17:48:50 +0200

    instalacion de primeflex y configuracion inicial de basicpage

--- src/app/products/pages/basics-page/basics-page.component.ts
+++ src/app/products/pages/basics-page/basics-page.component.ts
@@ -7,4 +7,8 @@ import { Component } from '@angular/core';
 })
 export class BasicsPageComponent {
 
+  public nameLower: string = 'tamara';
+  public nameUp: string = 'TAMARA';
+  public nameTitle: string = 'TaMaRa gArCiA';
+
 }
