8fc196765d50d3aafcb36c148e1ad55b529d9de9
Tamara García <tamaragarciamarcos@gmail.com>
2024-04-09 13:48:04 +0200

    fin de la seccion

--- src/app/products/pages/order/order.component.ts
+++ src/app/products/pages/order/order.component.ts
@@ -9,6 +9,7 @@ import { Color, Hero } from '../../interfaces/hero.interface';
 export class OrderComponent {
 
   public isUpperCase: boolean = false;
+  public orderBy?: keyof Hero | '' = '';
 
   public heroes: Hero[] = [
     {
@@ -53,5 +54,8 @@ export class OrderComponent {
     this.isUpperCase = !this.isUpperCase;
   }
 
+  changeOrder(value: keyof Hero){
+    this.orderBy = value
+  }
 
 }
